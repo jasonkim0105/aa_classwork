8549a25f5c2ac51b3fbe8d57d037561fbc5f276b
your name here <your_email_hereroelliga@yahoo.com>
2021-07-20 15:05:15 -0700

    make_cards & populate

--- w3d2/memory_puzzle/board.rb
+++ w3d2/memory_puzzle/board.rb
@@ -2,18 +2,27 @@ class Board
 
   attr_reader :grid
 
-  def initialize()
-    @grid = Array.new(4) {Array.new(4)}
+  def initialize(size)
+    @size = 4
+    @grid = Array.new(size) {Array.new(size)}
 
   end
 
-  def populate(*card)
+  def make_cards #should return an array of all the cards, shuffled
+    values = ("A".."Z").to_a
+
+  def populate # should place each card on a space on the grid
     i = 0
-    card = Card.new()
-    while i < 8
-      row = rand(0...grid.length)
-      col = rand(0...grid.length)
-      i += 1
+    j = 0
+    while j < 16
+        card = Card.new()
+        while i < 2
+            row = rand(0...grid.length)
+            col = rand(0...grid.length)
+            @grid[row][col] = card
+            i += 1
+        end
+        j += 1
     end
   end
 
--- w3d2/memory_puzzle/card.rb
+++ w3d2/memory_puzzle/card.rb
@@ -2,23 +2,26 @@ class Card
 
 attr_reader :face_value
 
-  def initialize(face_value, face_up, face_down)
+  def initialize(face_value)
     @face_value = face_value
-    @face_up = face_up
-    @face_down = face_down
-    @face_up_down = @face_up
+    @face_down = true
   end
 
-  def hide
-    if @face_up_down == @face_up
-      @face_up_down = @face_down
-    end
+  def hide #turns face_down into true
+    @face_down = true
   end
 
-  def reveal
-    @face_value
+  def reveal #flips the card, turns the face_down value into false
+    @face_down = false
   end
 
+  def to_s #if face_down is true return " " else return face_value
+    
+  end
+
+  def == #compare two cards to see they are the same and return true if so
+
+  end
 
   # def hide
   #   if !self.up_or_down
