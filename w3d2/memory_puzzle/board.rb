85a842236f42f80a4a882aa586d1b478e7b1144d
Jason <k.jason0105@gmail.com>
2021-07-20 14:33:27 -0700

    board

--- w3d2/memory_puzzle/board.rb
+++ w3d2/memory_puzzle/board.rb
@@ -1,4 +1,20 @@
 class Board
 
-    
+  attr_reader :grid
+
+  def initialize()
+    @grid = Array.new(4) {Array.new(4)}
+
+  end
+
+  def populate(*card)
+    i = 0
+    card = Card.new()
+    while i < 8
+      row = rand(0...grid.length)
+      col = rand(0...grid.length)
+      i += 1
+    end
+  end
+
 end
