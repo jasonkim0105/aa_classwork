97932b96a53afd6ce41fee938a226ea269b619c7
Jason <k.jason0105@gmail.com>
2021-07-21 11:56:06 -0700

    expo

--- w3d3/recursion_exercises.rb
+++ w3d3/recursion_exercises.rb
@@ -2,9 +2,40 @@ def range_recursive(start, last)
 
     return [] if last == start
 
-    [start] += range_recursive(start + 1, last)
-                            
+    [start] + range_recursive(start + 1, last)
+
 end
 
-p range_recursive(1,5)
+# p range_recursive(1,5)
 
+def exp(b, n) #
+  #exp (3, 5) -> 3 * exp(3,4)
+  #exp (3, 4) -> 3 * exp (3,3)
+  #exp (3, 3) -> 3 * exp(3,2)
+  #exp (3, 2) -> 3 * exp (3,1)
+  #exp (3, 1) -> 3 * exp(3,0)
+
+
+  return 1 if n == 0
+  return b if n == 1
+
+  if n % 2 == 0
+    exp(b, n/2) ** 2
+  else
+    b * (exp(b, (n-1) / 2) ** 2)
+  end
+
+  # exp(b, n/2) ** 2
+    #exp(2, 4) -> exp(2,2) ** 2
+                  #exp(2, 1) ** 2
+
+  # b * (exp(b, (n-1) / 2) ** 2)
+  #  3 * (exp (3, (3-1) / 2) ** 2)
+  #                 2 /2
+  #   3 * (exp (3, 1) ** 2)
+  #   3 *   3 ** 2 -> 27
+end
+
+# p exp(3, 4)
+# p exp(3, 5)
+# p exp(4, 4)
