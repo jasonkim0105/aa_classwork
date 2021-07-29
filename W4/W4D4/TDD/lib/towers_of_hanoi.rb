4ba6836c207df80d4526ede84dc6da3d43e18bbb
Darrick <darrickshin@gmail.com>
2021-07-29 14:23:07 -0700

    Finish Towers of Hanoi

--- W4/W4D4/TDD/lib/towers_of_hanoi.rb
+++ W4/W4D4/TDD/lib/towers_of_hanoi.rb
@@ -13,9 +13,40 @@ class Tower
         raise "raises error if the args are invalid" if
         start_pos < 1 || start_pos > 3 || end_pos < 1 || end_pos > 3
 
-        case
+        case start_pos
+        when 1
+            start_pos = @tower_1
+        when 2
+            start_pos = @tower_2
+        when 3
+            start_pos = @tower_3
+        end
 
+        case end_pos
+        when 1
+            end_pos = @tower_1
+        when 2
+            end_pos = @tower_2
+        when 3
+            end_pos = @tower_3
+        end
 
+        if start_pos.empty?
+            raise "start is empty"
+        elsif start_pos.last && end_pos.empty?
+            end_pos << start_pos.pop
+            return true
+        elsif start_pos.last > end_pos.last
+            return false
+        elsif start_pos.last < end_pos.last
+            end_pos << start_pos.pop
+            return true
+        end
+    end
+
+    def won
+        return true if @tower_3 == [3,2,1]
+        return false
     end
 
 end
--- W4/W4D4/TDD/spec/towers_of_hanoi_spec.rb
+++ W4/W4D4/TDD/spec/towers_of_hanoi_spec.rb
@@ -11,37 +11,54 @@ describe Tower do
     end
 
     describe "#move" do
-     describe "takes two parameters, one for the starting stack and one for the ending stack" do
-        context "with_invalid_args" do
-            it "raises error if the args are invalid" do
-                expect{tower.move(1)}.to raise_error(ArgumentError)
+        describe "takes two parameters, one for the starting stack and one for the ending stack" do
+            context "with_invalid_args" do
+                it "raises error if the args are invalid" do
+                    expect{tower.move(1)}.to raise_error(ArgumentError)
 
-                expect{tower.move("a",0)}.to raise_error("raises error if the args are invalid")
+                    expect{tower.move("a",0)}.to raise_error("raises error if the args are invalid")
 
-                expect{tower.move(0,"b")}.to raise_error("raises error if the args are invalid")
+                    expect{tower.move(0,"b")}.to raise_error("raises error if the args are invalid")
 
-                expect{tower.move(-1, 0)}.to raise_error("raises error if the args are invalid")
+                    expect{tower.move(-1, 0)}.to raise_error("raises error if the args are invalid")
 
-                expect{tower.move(4,0)}.to raise_error("raises error if the args are invalid" )
-                expect{tower.move(1,5)}.to raise_error("raises error if the args are invalid")
-                expect{tower.move(0,-4)}.to raise_error("raises error if the args are invalid")
+                    expect{tower.move(4,0)}.to raise_error("raises error if the args are invalid" )
+                    expect{tower.move(1,5)}.to raise_error("raises error if the args are invalid")
+                    expect{tower.move(0,-4)}.to raise_error("raises error if the args are invalid")
+                end
             end
-        end
-        context "with valid args" do
-            it "it should return false if top ring's value at end_pos stack is lower than top ring's value at start_pos" do
-                tower.move(1,2)
-                expect(tower.move(1,2)).to eq false
+            context "with valid args" do
+                it "it should return false if top ring's value at end_pos stack is lower than top ring's value at start_pos" do
+                    tower.move(1,2)
+                    expect(tower.move(1,2)).to eq false
 
 
-            end
-            it "if the end_pos is empty or end_pos's value is greater than start_pos value, then move start_pos into end_pos and return true" do
-                tower.move(1,2)
-                expect(tower.move(1,3)).to eq true
-                expect(tower.move(2,3)).to eq true
-                expect(tower.instance_variable_get("@tower_3")).to eq [1,2]
+                end
+                it "if the end_pos is empty or end_pos's value is greater than start_pos value, then move start_pos into end_pos and return true" do
+                    tower.move(1,2)
+                    expect(tower.move(1,3)).to eq true
+                    expect(tower.move(2,3)).to eq true
+                    expect(tower.instance_variable_get("@tower_3")).to eq [2,1]
+                end
             end
         end
     end
+
+    describe "#won" do
+        it "return true if all rings are on the last stack" do
+            tower.move(1,3)
+            tower.move(1,2)
+            tower.move(3,2)
+            tower.move(1,3)
+            tower.move(2,1)
+            tower.move(2,3)
+            tower.move(1,3)
+            expect(tower.won).to eq true
+        end
+
+        it "return false if all rings are not on the last stack" do
+            expect(tower.won).to eq false
+        end
     end
 
 
