4794693a48254c706015df7af62e8e541970c896
Jason <k.jason0105@gmail.com>
2021-07-30 13:32:41 -0700

    started w4d5

---
+++ W4/W4D5/execution_time.rb
@@ -0,0 +1,37 @@
+def my_min(array)
+  # min = 0
+  # array.each do |num|
+  #   min = num if num  <  min
+  # end
+  # min
+  min = array.first
+  array.each do |num|
+    newArray = array[0 + 1.. -1]
+    newArray.each do |otherNums|
+      if otherNums < min
+        min = otherNums
+      end
+    end
+  end
+  min
+end
+
+# list = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
+# p my_min(list)  # =>  -5
+
+def largest_contiguous_subsum(array)
+  newArr = []
+  (0...array.length).each do |i|
+    (0...array.length).each do |j|
+      newArr << array[i..j]
+    end
+  end
+  max = 0
+  newArr.each do |subArray|
+    max = subArray.sum if subArray.sum > max
+  end
+  max
+end
+
+list = [5, 3, -7]
+p largest_contiguous_subsum(list) # => 8
