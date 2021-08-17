c50ed4396161bbbac16f083e715ec50dd5ac6c55
IanDMcGrath <ian92415@gmail.com>
2021-08-16 19:07:06 -0700

    tried to bug fix

--- w7/W7D1/cats 99 auth/app/controllers/sessions_controller.rb
+++ w7/W7D1/cats 99 auth/app/controllers/sessions_controller.rb
@@ -1,14 +1,21 @@
 class SessionsController < ApplicationController
+  before_action  :require_logged_out, only: [:new, :create]
+  before_action :require_logged_in, only: [:destroy]
+  before_action :require_i_own_this_cat, only: [:edit, :update]
+  
   def new
     @user = User.new
     render :new
   end
 
   def create
+    puts "username: #{params[:user][:username]}"
+    puts "password: #{params[:user][:password]}"
     @user = User.find_by_credentials(params[:user][:username], params[:user][:password])
-
+    puts "this is beans"
+    puts @user.username
     if @user
-      login(@user)
+      login_user!(@user)
       redirect_to cats_url
     else
       render :new
@@ -19,10 +26,6 @@ class SessionsController < ApplicationController
     logout!
     redirect_to new_session_url
   end
-
-  before_action  :require_logged_out, only: [:new, :create]
-  before_action :require_logged_in, only: [:destroy]
-  before_action :require_i_own_this_cat, only: [:edit, :update]
 
 
 end
--- w7/W7D1/cats 99 auth/app/models/user.rb
+++ w7/W7D1/cats 99 auth/app/models/user.rb
@@ -6,8 +6,8 @@ class User < ApplicationRecord
 
   attr_reader :password
 
-  before_validation :ensure_session_token!
-  # after_initialize :ensure_session_token!
+  # before_validation :ensure_session_token!
+  after_initialize :ensure_session_token!
 
   has_many :cats,
     foreign_key: :user_id,
@@ -21,11 +21,7 @@ class User < ApplicationRecord
 	  self.password_digest = BCrypt::Password.create(password)
   	@password = password
   end
-
-  def ensure_session_token!
-    self.session_token ||= SecureRandom::urlsafe_base64(16)
-  end
-
+  
   def self.find_by_credentials(username, password)
     user = User.find_by(username: username)
     if user && user.is_password?(password)
@@ -34,18 +30,21 @@ class User < ApplicationRecord
       return nil
     end
   end
-
+  
   def is_password?(password)
     password_object = BCrypt::Password.new(self.password_digest)
     password_object.is_password?(password)
   end
-
+  
   def reset_session_token!
     self.session_token = SecureRandom::urlsafe_base64(16)
     self.save!
     self.session_token
   end
-
+  
+  def ensure_session_token!
+    self.session_token ||= SecureRandom::urlsafe_base64(16)
+  end
 
 
 end
