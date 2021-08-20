5a2494a3abae371741c902b848a02b2108d1fc7b
Jason <k.jason0105@gmail.com>
2021-08-20 10:09:24 -0700

    users controller

--- w7/W7D5/RedditClone/app/controllers/users_controller.rb
+++ w7/W7D5/RedditClone/app/controllers/users_controller.rb
@@ -1,2 +1,20 @@
 class UsersController < ApplicationController
+
+  def create
+    @user = User.new(user_params)
+    if @user
+      @user.save
+      redirect_to
+  end
+
+  def new
+    @user = User.new
+    render :new
+  end
+
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
 end
