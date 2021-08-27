c191acaddcd816b3d43cc1454ab8434dd631b2fc
Jason <k.jason0105@gmail.com>
2021-08-26 17:31:00 -0700

    tictactoe start

---
+++ W8/W8D4/tictactoe/board.js
@@ -0,0 +1,20 @@
+class Board {
+  constructor() {
+    let grid = Array(3).fill().map(() => Array(3).fill());
+  }
+
+  won (){
+
+  }
+
+  winner() {
+
+  }
+  empty(pos) {
+
+  }
+
+  place_mark(pos, mark) {
+
+  }
+}
