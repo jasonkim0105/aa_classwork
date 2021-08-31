aa9663b5b8b1c06a693d9bcf976a42ecef9761e0
Jason <k.jason0105@gmail.com>
2021-08-31 14:57:12 -0700

    ttt-brower fun

--- W9/W9D2/Brower-TTT/src/index.js
+++ W9/W9D2/Brower-TTT/src/index.js
@@ -1,6 +1,9 @@
-const View = // require appropriate file
-const Game = // require appropriate file
+const View = require("./ttt-view");
+const Game = require("../ttt_node/game");
 
 document.addEventListener("DOMContentLoaded", () => {
-  // Your code here
+  const game = new Game();
+  let el = document.querySelector(".ttt")
+  const view = new View(game, el);
+
 });
--- W9/W9D2/Brower-TTT/src/ttt-view.js
+++ W9/W9D2/Brower-TTT/src/ttt-view.js
@@ -1,8 +1,39 @@
 class View {
-  constructor(game, el) {}
+  constructor(game, el) {
+    this.game = game;
+    this.el = el;
+    this.setupBoard();
+  }
 
-  setupBoard() {}
-  
+  setupBoard() {
+    const ul = document.createElement("ul");
+    // const li = document.createElement("li");
+    for (let i = 0; i < 9; i++){
+      let li = document.createElement("li");
+      li.setAttribute("data-pos", i);
+      li.style.display = "inline-block";
+      li.style.width = "50px";
+      li.style.height = "50px";
+      li.style.borderStyle = "solid";
+      li.style.background = "gray";
+      li.style.borderWidth = "1px";
+      ul.append(li);
+    }
+    this.el.append(ul);
+    ul.style.display = "flex";
+    ul.style.width = "159px";
+    ul.style.flexWrap = "wrap";
+    ul.addEventListener("mouseover", (e) => {
+      let ele = e.target;
+      ele.style.background = "yellow";
+    });
+    ul.addEventListener("mouseout", (e) => {
+      let ele = e.target;
+      ele.style.background = "gray";
+    });
+
+  }
+
   bindEvents() {}
 
   handleClick(e) {}
@@ -10,5 +41,6 @@ class View {
   makeMove(square) {}
 
 }
+
 
 module.exports = View;
